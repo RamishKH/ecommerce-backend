b1c989b6649fde1ecb80e547340e88b7996debef
RamishKH <kramish241@gmail.com>
2024-04-07 03:43:52 +0500

    before overwrite

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const cors = require('cors');
+
+const productsRouter = require('./routes/products');
+const orderRouter = require('./routes/order');
 
 const app = express();
+
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'PUT', 'DELETE', 'PATCH', 'POST'],
+    allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With, Accept'
+}));
 
 app.get('/', (req, res) => {
   res.send('Successful response.');
 });
+
+
+app.use('/products', productsRouter);
+app.use('/orders', orderRouter);
+
 
 
 app.listen(3000, () => console.log(`Example app is listening on ${process.env} 3000.`));
